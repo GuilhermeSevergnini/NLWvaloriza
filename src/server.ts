20ab34dbb760eca73532227c48d445a13307daae
GuilhermeSevergnini <guilherme.severgnini@combateafraude.com>
2021-06-22 10:52:44 -0300

    Feat(dia 2 NLW): upando projeto dia 2

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,15 @@
+import "reflect-metadata";
 import express from "express";
 
-// @types/express
+import { router } from "./routes";
+
+import "./database";
+
+
 const app = express();
 
-/**
- * GET => Buscar informação
- * POST => inserir uma informação na API
- * PUT => Alterar uma inforamação (dados, nomes, endereço, produto etc)
- * DELETE => Remover informação
- * PATCH => Alterar informação especifica 
- */
+app.use(express.json());
 
-app.get("/test", (request, response) => {
-    // Request = entrando
-    // Response = saindo
-    return response.send("Olá NLW");
-});
+app.use(router);
 
-app.post("/test-post", (request, response) => {
-    return response.send("Olá NLW test post");
-
-});
-
-//http://localhost:3000
 app.listen(3000, () => console.log("Server is running"));
