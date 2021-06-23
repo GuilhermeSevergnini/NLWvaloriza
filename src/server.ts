86025cbd250e6a0e7ca385c06cd4782ebcb9a6ee
GuilhermeSevergnini <guilherme.severgnini@combateafraude.com>
2021-06-23 19:00:09 -0300

    Feat Atualização aula 3 NLWValoriza

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 
 import { router } from "./routes";
 
@@ -11,5 +11,19 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return response.status(400).json({
+            error: err.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: "internal Server Error"
+    })
+
+})
 
 app.listen(3000, () => console.log("Server is running"));
